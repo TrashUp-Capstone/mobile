d61576c6a0afc1d3ec748228cfae2a3dba10bf1c
gochujjang <41176663+gochujjang@users.noreply.github.com>
2024-06-02 22:55:34 +0700

    merging

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,6 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.play.services.maps)
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
